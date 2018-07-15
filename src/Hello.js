33c803c1abbcd248d67c5b9c03771d457fe3f448
Robert Harężlak <harezlak.robert7@gmail.com>
2018-07-15 19:25:29 +0200

    First react component

---
+++ src/Hello.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './hello.css'
+
+class Hello extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World Cruel React</h1>
+        <p>Welcome to React</p>
+      </div>
+    )
+  }
+}
+
+export default Hello;
