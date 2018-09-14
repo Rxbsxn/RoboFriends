d8feadfd2b355904223c46e72f48697eea99cecc
Robert Harężlak <harezlak.robert7@gmail.com>
2018-09-14 19:16:07 +0200

    code optimaliztion

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -5,6 +5,7 @@ const SearchBox = ({ onSearchChange }) => {
     <div className='pa2'>
       <input 
         className='pa3 ba b--green bg-lightest-blue'
+        aria-label='Search robots'
         type='search'
         placeholder='search for robots'
         onChange={onSearchChange}
