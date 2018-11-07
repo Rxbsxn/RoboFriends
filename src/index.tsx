8a43b9cc131c38da6fcd5cc417d32bd40d804108
Robert Harężlak <harezlak.robert7@gmail.com>
2018-11-07 22:22:04 +0100

    setup for tslint

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import 'tachyons';
 import './index.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import App from './containers/App';
 import { createLogger } from "redux-logger";
