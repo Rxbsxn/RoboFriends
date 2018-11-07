33e90005c5e442bba22f8dd94b94a529ca8c3dbe
Robert Harężlak <harezlak.robert7@gmail.com>
2018-11-07 23:05:51 +0100

    SSR

---
+++ src/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import React from 'react';
+import fs from 'fs';
+
+import { renderToString } from 'react-dom/server';
+import App from './containers/App';
+
+const app = express();
+
+app.use(express.static('../public'));
+
+const robots = JSON.parse(fs.readFileSync('../public/robots.json', 'utf8'));
+
+const roboFriendsApp = React.createElement(App);
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    content: renderToString(roboFriendsApp({ data: robots }))
+  });
+});
